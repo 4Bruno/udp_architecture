410c5b4d9593435a14d08c26445a0a4005ca03da
Ubuntu <carlosginescarretero@gmail.com>
2022-07-17 11:52:26 +0000

    Only linux only create server

--- build.sh
+++ build.sh
@@ -10,4 +10,4 @@ fi
 serious_c_flags="-m64 -pedantic -Wshadow -Wpointer-arith -Wcast-qual"
 # -std=gnu11
 echo "Building test network server"
-gcc $serious_c_flags -Wall -DDAEMON=0 -DDEBUG=0 -DDISABLE_STDIO=0  -ggdb src/udp_server.cpp src/linux_network_udp.cpp -o build/debug/udp_server.exe
+gcc $serious_c_flags -Wall -DDAEMON=0 -DDEBUG=0 -DDISABLE_STDIO=0  -ggdb  src/linux_time.cpp src/udp_server.cpp src/linux_network_udp.cpp src/MurmurHash3.cpp -o build/debug/udp_server.exe
